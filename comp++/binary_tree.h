4b8269245e8e4e7c7ec6ed48cc4fe0bbb94db490
JG <jgarcia@iot.es>
2024-04-01 16:22:18 +0200

    bst

--- comp++/binary_tree.h
+++ comp++/binary_tree.h
@@ -1,6 +1,9 @@
 #ifndef BINARY_TREE_H
 #define BINARY_TREE_H
 
+#include "queue.h"
+#include "stack.h"
+
 template<typename T>
 class BSTNode
 {
@@ -26,6 +29,79 @@ public:
 		}
 		return 0;
 	}
+
+	// -- Traversals
+	// Breath-first: top-down and lef-right
+	void breadthFirst() {
+		Queue<node*> queue;
+		node* p = root;
+		while (p != 0) {
+			queue.enqueue(p);
+			while (!queue.isEmpty()) {
+				p = queue.dequeue();
+				// visit(p)
+				if (p->left != 0) queue.enqueue(p->left);
+				if (p->right != 0) queue.enqueue(p->right);
+			}
+		}
+	}
+	// Depth-first: proceeds all the way to the left then backs up to 
+	// the first intersection and chooses the other side if it exists
+	// and goes all the way again. Types:
+	// VLR: visit left right (preorder)
+	// LVR: left visit right (inorder)
+	// LRV: left right visit (postorder)
+	// and all the combinations for example RVL, VRL...
+	// Where V=visiting the node, and L, R = traversing the left or right
+	// subtree
+
+	// Recursive implementation
+	void inorder(node* p) {
+		if (p != 0) {
+			inorder(p->left);
+			// visit(p);
+			inorder(p->right);
+		}
+	}
+	void preorder(node* p) {
+		if (p != 0) {
+			//visit(p);
+			preorder(p->left);
+			preorder(p->right);
+		}
+	}
+	void postorder(node* p) {
+		if (p != 0) {
+			postorder(p->left);
+			postorder(p->right);
+			//visit(p);
+		}
+	}
+	// Stack implementation
+	void inorder() {
+		node* p = root;
+		Stack<node*> stack;
+		while (p != 0) {
+			while (p != 0) {
+				if(p->right!=0)
+					stack.push(p->right);
+				stack.push(p);
+				p = p->left;
+			}
+			p = stack.pop(); // first node with no left 
+			while (p != 0 && p->right == 0) {
+				// visit(p);
+				p = stack.pop();
+			}
+			// first node with a right
+			// visit(p)
+			if (!stack.isEmpty()) {
+				p = stack.pop();
+			}
+			else p = 0;
+		}
+	}
+
 private:
 	typedef BSTNode<T> node;
 	node* root;
---
+++ comp++/stack.h
@@ -0,0 +1,31 @@
+#ifndef STACK_H
+#define STACK_H
+#include <vector>
+
+template<class T, int capacity = 30>
+class Stack {
+public:
+	Stack() {
+		pool.reserve(capacity);
+	}
+	void clear() {
+		pool.clear();
+	}
+	bool isEmpty() const {
+		return pool.empty();
+	}
+	T& topEl() {
+		return pool.back();
+	}
+	T pop() {
+		T el = pool.back();
+		pool.pop_back();
+		return el;
+	}
+	void push(const T& el) {
+		pool.push_back(el);
+	}
+private:
+	vector<T> pool;
+};
+#endif
